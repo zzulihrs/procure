5c0927c4ac0c795b8d1425c77047ea340e059c3f
hrs <2418838198.com>
2023-05-30 19:05:42 +0800

    element

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import Element from 'element-ui';
+Vue.use(Element, { size: 'small', zIndex: 3000 });
 
 Vue.config.productionTip = false
 
